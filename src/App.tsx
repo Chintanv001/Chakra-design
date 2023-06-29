82be86436c520447cad5febb164f9f07037041e9
Chintan1812 <chintanvekariyaofficial@gmail.com>
2023-06-29 21:50:46 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import { Features } from './Components/Features'
+import Header from './Components/Header'
+import Pricing from './Components/Pricing'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <Header/>
+      <Pricing/>
+      <Features/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
